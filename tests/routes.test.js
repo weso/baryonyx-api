1a0dd8314ced8195e089d570033cac81a7931232
jorgealvarezfidalgo <uo258524@uniovi.es>
2020-01-30 17:45:02 +0100

    Añadida respuesta esperada en los test de lectura

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -43,7 +43,15 @@ describe('Testing the API', () => {
     expect(res.body).not.toHaveProperty('error')
     const id = '45678G'
     expect(res.type).toEqual('application/json')
-    // expect(res.json).toContain('Alergia')
+    expect(res.json[0]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/VaU0020aU0020morir')
+    expect(res.json[0]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#1')
+    expect(res.json[0]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00201')
+    expect(res.json[1]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/AU0020verU0020siU0020funciona')
+    expect(res.json[1]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#2')
+    expect(res.json[1]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00202')
+    expect(res.json[2]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/NoU0020puedeU0020respirarU0020bien')
+    expect(res.json[2]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#3')
+    expect(res.json[2]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00203')
   })
 
   // updating and adding new allergies to the user -- POST
@@ -54,7 +62,7 @@ describe('Testing the API', () => {
         "idcl": "45678G",
         "idal": ["1", "2", "8"],
         "allergy": ["Alergia 1 CAMBIADA", "Alergia 2 CAMBIADA", "Alergia 8"],
-        "description": ["Va a morir", "A ver si funciona", "Todo bien"]
+        "description": ["Fallo múltiple", "A ver si funciona", "Todo bien"]
       })
     expect(res.statusCode).toEqual(201)
     expect(res.body).toHaveProperty('message')
@@ -70,7 +78,18 @@ describe('Testing the API', () => {
     expect(res.body).not.toHaveProperty('error')
     const id = '45678G'
     expect(res.type).toEqual('application/json')
-    // expect(res.json).toContain('Alergia')
+    expect(res.json[0]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/FalloU0020múltiple')
+    expect(res.json[0]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#1')
+    expect(res.json[0]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00201U0020CAMBIADA')
+    expect(res.json[1]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/AU0020verU0020siU0020funciona')
+    expect(res.json[1]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#2')
+    expect(res.json[1]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00202U0020CAMBIADA')
+    expect(res.json[2]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/NoU0020puedeU0020respirarU0020bien')
+    expect(res.json[2]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#3')
+    expect(res.json[2]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00203')
+    expect(res.json[3]['?descripcion'].value).toContain('https://oth2.solid.community/symmetry/45678G/TodoU0020bien')
+    expect(res.json[3]['?id'].value).toContain('https://oth2.solid.community/symmetry/45678G/Alergias.ttl#8')
+    expect(res.json[3]['?nombre'].value).toContain('https://oth2.solid.community/symmetry/45678G/AlergiaU00208')
   })
 
   it('Deleting the allergy user file', async () => {
